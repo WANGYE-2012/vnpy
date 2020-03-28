f0843cd01fbca75576c8fea42d501fafcae9d3ea
wangye2018 <1175208935@qq.com>
2020-03-28 20:20:06 +0800

    first

---
+++ examples/JQDataService/dataService.py
@@ -0,0 +1,112 @@
+# encoding: UTF-8
+
+from __future__ import print_function
+import json
+import time as ti
+import datetime as dt
+import random
+from datetime import datetime, timedelta
+
+from pymongo import MongoClient, ASCENDING
+from jqdatasdk import *
+from vnpy.trader.database import database_manager
+from vnpy.trader.object import BarData
+from vnpy.trader.constant import Interval,Exchange
+import os
+# 加载配置
+Path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'config.json')
+config = open(Path)
+setting = json.load(config)
+
+MONGO_HOST = setting['MONGO_HOST']
+MONGO_PORT = setting['MONGO_PORT']
+JQ_IP = setting['JQ_IP']
+JQ_Password = setting['JQ_Password']
+SYMBOLS = setting['SYMBOLS']
+
+auth(JQ_IP, JQ_Password)      # 历史行情服务API对象
+
+# 数据库
+# ----------------------------------------------------------------------
+def generateExchange(symbol):
+    """生成JQ合约代码"""
+    if symbol[0:2] in ['cu', 'al', 'zn', 'pb', 'ni', 'sn', 'au', 'ag', 'rb', 'wr', 'hc', 'ss', 'fu', 'bu', 'ru', 'sp']:
+        exchangesymbol = symbol.upper() + '.XSGE'
+    elif symbol[0:2] in ['m2', 'y2', 'a2', 'b2', 'p2', 'c2', 'cs', 'jd', 'bb', 'fb', 'l2', 'v2', 'eg', 'pp', 'j2', 'jm',
+                         'I2', 'EB']:
+        exchangesymbol = symbol.upper() + '.XDCE'
+    elif symbol[0:2] in ['SR', 'CF', 'CY', 'ZC', 'FG', 'TA', 'MA', 'WH', 'PM', 'LR', 'JR', 'OI', 'RM', 'SF', 'SM', 'AP',
+                         'RU', 'SA', 'SJ']:
+        exchangesymbol =symbol[0:2] + '2' + symbol[2:5] + '.XZCE'
+    return  exchangesymbol
+
+
+# ----------------------------------------------------------------------
+def Exchange1(symbol):
+    """返回交易所代码"""
+    if symbol[0:2] in ['cu', 'al', 'zn', 'pb', 'ni', 'sn', 'au', 'ag', 'rb', 'wr', 'hc', 'ss', 'fu', 'bu', 'ru', 'sp']:
+        exchange1 = 'SHFE'
+    elif symbol[0:2] in ['m2', 'y2', 'a2', 'b2', 'p2', 'c2', 'cs', 'jd', 'bb', 'fb', 'l2', 'v2', 'eg', 'pp', 'j2', 'jm',
+                         'I2', 'EB']:
+        exchange1 = 'DCE'
+    elif symbol[0:2] in ['SR', 'CF', 'CY', 'ZC', 'FG', 'TA', 'MA', 'WH', 'PM', 'LR', 'JR', 'OI', 'RM', 'SF', 'SM', 'AP',
+                         'RU', 'SA', 'SJ']:
+        exchange1 = 'CZCE'
+    return exchange1
+# ----------------------------------------------------------------------
+def generateVtBar(symbol,ix,d):
+    """生成K线"""
+    bar = BarData(
+        symbol=symbol,
+        exchange=Exchange(Exchange1(symbol)),
+        datetime=ix,
+        interval=Interval.MINUTE,
+        volume=d["volume"],
+        open_price=d["open"],
+        high_price=d["high"],
+        low_price=d["low"],
+        close_price=d["close"],
+        gateway_name='DB',
+    )
+    return bar
+
+#----------------------------------------------------------------------
+def downMinuteBarBySymbol(symbol, num):
+    """下载某一合约的分钟线数据"""
+    start = ti.time()
+
+    df = get_price(generateExchange(symbol), count=num,  frequency='1m',
+                   fields=[ 'open', 'high', 'low', 'close', 'volume', 'open_interest'],
+                   end_date=dt.datetime.now())
+    if df.empty:
+        print(u'%s数据下载失败' %symbol)
+        return
+
+    bars = []
+    for ix, row in df.iterrows():
+        bar = generateVtBar(symbol,ix,row)
+        bars.append(bar)
+
+    database_manager.save_bar_data(bars)
+
+    end = ti.time()
+    cost = (end - start) * 1000
+
+    print(u'合约%s数据下载完成%s - %s，耗时%s毫秒' %(symbol, df.index[0], df.index[-1], cost))
+# ----------------------------------------------------------------------
+def downloadAllMinuteBar(num):
+    """下载所有配置中的合约的分钟线数据"""
+    print('-' * 50)
+    print(u'开始下载合约分钟线数据')
+    print('-' * 50)
+    
+    for symbol in SYMBOLS:
+        downMinuteBarBySymbol(symbol, num)
+        ti.sleep(1)
+
+    print('-' * 50)
+    print(u'合约分钟线数据下载完成')
+    print('-' * 50)
+
+
+    
---
+++ vnpy/app/cta_strategy/strategies/tick_strategy.py
@@ -0,0 +1,109 @@
+from vnpy.app.cta_strategy import (
+    CtaTemplate,
+    StopOrder,
+    TickData,
+    BarData,
+    TradeData,
+    OrderData,
+    BarGenerator,
+    ArrayManager,
+)
+import datetime
+from datetime import time
+
+class TickStrategy(CtaTemplate):
+    author = "WangYe"
+
+    fast_window = 10
+    slow_window = 20
+
+    a = 0
+    b = 0
+
+    day_open = 0.0
+    exit_time = time(hour=14, minute=55)
+
+    parameters = ["fast_window", "slow_window"]
+    variables = ["day_open"]
+
+    def __init__(self, cta_engine, strategy_name, vt_symbol, setting):
+        """"""
+        super().__init__(cta_engine, strategy_name, vt_symbol, setting)
+        self.bg = BarGenerator(self.on_bar)
+        self.am = ArrayManager()
+
+    def on_init(self):
+        """
+        Callback when strategy is inited.
+        """
+        self.write_log("策略初始化")
+        self.load_bar(10)
+
+    def on_start(self):
+        """
+        Callback when strategy is started.
+        """
+        self.write_log("策略启动")
+        # 范围时间
+        d_time = datetime.datetime.strptime(str(datetime.datetime.now().date()) + '8:55', '%Y-%m-%d%H:%M')
+        d_time1 = datetime.datetime.strptime(str(datetime.datetime.now().date()) + '20:59', '%Y-%m-%d%H:%M')
+
+        # 当前时间
+        n_time = datetime.datetime.now()
+
+        # 判断当前时间是否在范围时间内
+        if n_time > d_time and n_time < d_time1:
+            if self.day_open:
+                self.buy(self.day_open * 1.04, 1)
+                print(self.day_open)
+        self.put_event()
+
+    def on_stop(self):
+        """
+        Callback when strategy is stopped.
+        """
+        self.write_log("策略停止")
+        self.put_event()
+
+    def on_tick(self, tick: TickData):
+        """
+        Callback of new tick data update.
+        """
+        if self.pos > 0 and self.a == 0:
+            self.sell(tick.open_price + 2, abs(self.pos))
+            self.a = 1
+        elif self.pos < 0 and self.b == 0:
+            self.cover(tick.open_price - 2, abs(self.pos))
+            self.b = 1
+        self.bg.update_tick(tick)
+
+    def on_bar(self, bar: BarData):
+        """
+        Callback of new bar data update.
+        """
+
+        self.day_open = bar.close_price
+        if bar.datetime.time() > self.exit_time:
+            self.cancel_all()
+            if self.pos > 0:
+                self.sell(bar.close_price * 0.99, abs(self.pos))
+            elif self.pos < 0:
+                self.cover(bar.close_price * 1.01, abs(self.pos))
+
+    def on_order(self, order: OrderData):
+        """
+        Callback of new order data update.
+        """
+        pass
+
+    def on_trade(self, trade: TradeData):
+        """
+        Callback of new trade data update.
+        """
+        self.put_event()
+
+    def on_stop_order(self, stop_order: StopOrder):
+        """
+        Callback of stop order update.
+        """
+        pass
